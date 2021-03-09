c007bc8bf0b8e347af6118c269ca45026db1a7d4
Alejandra lopez pineda <al.lopezpineda@gmail.com>
2021-03-09 19:29:34 +0100

    Basic Infrastructure created.

---
+++ EGym.Logic/Infrastructure/IDataManager.cs
@@ -0,0 +1,22 @@
+﻿using EGym.Logic.Models.People;
+using EGym.Logic.Models.Relations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EGym.Logic.Infrastructure
+{
+    public interface IDataManager
+    {
+        bool RegisterClientInSystem(Client client);
+        bool DeleteClient(Client client);
+
+        bool RegisterActivity(Booking booking);
+        bool CancelActivity(Booking booking);
+
+
+        
+    }
+}
