b895e276aa6e3c359acb0faa2db00f8cc611799f
Alejandra lopez pineda <al.lopezpineda@gmail.com>
2021-03-09 18:54:09 +0100

    Activity class created

---
+++ EGym.Logic/Models/Activity/Activity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EGym.Logic.Models.Activity
+{
+    public class Activity
+    {
+        public DateTime StartTime { get; set; }
+        public DateTime BookingDate { get; set; }
+        public int Duration { get; set; }
+
+        public Activity(DateTime startTime, DateTime bookningDate, int duration)
+        {
+            this.StartTime = startTime;
+            this.BookingDate = bookningDate;
+            this.Duration = duration;
+        }
+    }
+}
