18bbdabce81e141919c035072cb7b0d514a977b9
Alejandra lopez pineda <al.lopezpineda@gmail.com>
2021-03-09 20:27:39 +0100

    Booking class updated and EGym CheckingTrainerAvailability created.

--- EGym.Logic/EGym.cs
+++ EGym.Logic/EGym.cs
@@ -15,8 +15,11 @@ namespace EGym.Logic
         public IEnumerable<Activity> Activities { get; set; }
         public IEnumerable<Trainer> Trainers { get; set; }
         public IEnumerable<Client> Clients { get; set; }
+        public IEnumerable<Booking> Bookings { get; set; }
 
         public IDataManager DataManager { get; set; }
+
+        
 
 
 
@@ -25,6 +28,16 @@ namespace EGym.Logic
             
             DataManager = new DataManagerSimulator();
 
+        }
+
+        public bool CheckTrainerAvailability(int trainerId, DateTime startDate, DateTime endDate)
+        {
+            var result = Bookings.FirstOrDefault(b => (b.TrainerId == trainerId) && Utility.CheckDate(b.StartTime, b.EndTime, startDate, endDate));
+            if (result != null)
+            {
+                return false;
+            }
+            return true;
         }
 
        
--- EGym.Logic/Models/Relations/Booking.cs
+++ EGym.Logic/Models/Relations/Booking.cs
@@ -8,5 +8,10 @@ namespace EGym.Logic.Models.Relations
 {
     public class Booking
     {
+        public int TrainerId { get; internal set; }
+        public int ActivityId { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        
     }
 }
