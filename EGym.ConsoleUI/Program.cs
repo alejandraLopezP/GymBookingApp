902be20d1e25023595d8b6d9624b58d9ffc266a5
Alejandra lopez pineda <al.lopezpineda@gmail.com>
2021-03-10 21:31:49 +0100

    Inserting client througth ConsoleUI.

--- EGym.ConsoleUI/Program.cs
+++ EGym.ConsoleUI/Program.cs
@@ -14,8 +14,9 @@ namespace EGym.ConsoleUI
             Logic.EGym gym = new Logic.EGym();
             while (true)
             {
-                Console.WriteLine("Press R to Register a New Activity:  ");
+                Console.WriteLine("Press R to Register a New Client:  ");
                 Console.WriteLine("Press S to see the Gym schedule: ");
+                Console.WriteLine("Press L to get all clients: ");
                 Console.WriteLine("Press Q to Exit the system: ");
 
                 var pressedKey = Console.ReadLine();
@@ -23,10 +24,30 @@ namespace EGym.ConsoleUI
                 switch (pressedKey)
                 {
                     case "R":
+                        Console.WriteLine("Introduce Name: ");
+                        string name = Console.ReadLine();
+                        Random r = new Random();
+                        int id = r.Next(10000);
+                        bool wasPossible = gym.RegisterClient(id,name);
+                        if (wasPossible)
+                        {
+                            Console.WriteLine("Client create successfully");
+                        }
+                        else
+                        {
+                            Console.WriteLine("The was an error in the system");
+                        }
                         break;
                     case "S":
                         var result = gym.ShowSchedule();
                         Console.WriteLine(result);
+                        break;
+                    case "L":
+                        var clients = gym.ListAllClients();
+                        foreach (var item in clients)
+                        {
+                            Console.WriteLine("ClientId: {0} , Client Name: {1}", item.Id, item.Name);
+                        }
                         break;
                     case "Q":
                         Console.WriteLine("Thanks for using our App!");
